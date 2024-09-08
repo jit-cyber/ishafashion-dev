c071533e8978bbe4aec85d0ccae2a61839e08f65
jit-cyber <jitukumr@gmail.com>
2024-09-08 19:16:11 +0530

    in

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 <html lang="en">
 <?php include 'includes/head.php'; ?>
 <body>
-    
+
 <section class="header-section" id="header-placeholder">
     <?php include 'includes/header.php'; ?>
 </section>
 
 <?php
-    // Get the current URL path
-    $currentPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    // Enable error reporting for debugging
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    // Get the current URL path and remove trailing slashes
+    $currentPath = rtrim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), '/');
 
     // Define the default page
     $defaultPage = '/about';
@@ -25,6 +30,11 @@
 
     // Determine the file to include based on the current path
     $fileToInclude = isset($pageMap[$currentPath]) ? $pageMap[$currentPath] : $pageMap[$defaultPage];
+
+    // Fallback to a 404 page if the file does not exist
+    if (!file_exists($fileToInclude)) {
+        $fileToInclude = '404.php';
+    }
 ?>
 
 <main>
@@ -34,3 +44,4 @@
 <?php include 'includes/foot.php'; ?>
 </body>
 </html>
+
